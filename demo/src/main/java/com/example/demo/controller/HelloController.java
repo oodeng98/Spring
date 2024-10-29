333fbb5c40c6c3803ed17815944f5c9c57a1b668
oodeng98 <oodeng98@naver.com>
2024-10-30 01:29:55 +0900

    학습 진도 업로드

---
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name != null ? name : "Guest"); // 기본값 설정
+        return "hello-template";
+    }
+
+
+}
